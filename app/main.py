13f095d67bd06970c7dcae11e02cc002a5f6bf7a
mufhatu <mufhatusalemm@gmail.com>
2026-04-16 21:01:43 +0200

    fix: import issues

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from .schemas import DeveloperMetrics, PredictionResult
-from .model import predict_burnout
+from app.schemas import DeveloperMetrics, PredictionResult
+from app.model import predict_burnout
 
 app = FastAPI(title=" Developer Burnout Predictor")
 
